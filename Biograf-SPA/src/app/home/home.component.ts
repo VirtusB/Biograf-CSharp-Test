85fca67e6329b8f15b8d0d95ce25961b0f885838
VirtusDesktopPC <vbo@rastec.dk>
2018-11-28 20:53:35 +0100

    rettelser

--- Biograf-SPA/src/app/home/home.component.ts
+++ Biograf-SPA/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CarouselConfig } from 'ngx-bootstrap/carousel';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,7 @@ import { CarouselConfig } from 'ngx-bootstrap/carousel';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public authService: AuthService) { }
 
   ngOnInit() {
 
