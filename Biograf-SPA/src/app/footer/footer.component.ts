bdd789e67e407373041a370943e713e0196250e5
VirtusDesktopPC <vbo@rastec.dk>
2018-11-30 02:40:28 +0100

    film slider er nu kun på forsiden

--- Biograf-SPA/src/app/footer/footer.component.ts
+++ Biograf-SPA/src/app/footer/footer.component.ts
@@ -10,11 +10,24 @@ import { MovieService } from '../_services/movie.service';
 export class FooterComponent implements OnInit {
   movies: Movie[];
 
+
   constructor(private movieService: MovieService) { }
 
   ngOnInit() {
     this.getFooterSliderMovies();
+    setTimeout(() => {
+      document.querySelector('#slider-footer > marquee').start();
+    }, 500);
+    // let i = 0;
+    // let interval = setInterval(() => {
+    //   document.querySelector('#slider-footer > marquee').start();
+    //   i++;
+    //   if (i === 1) {
+    //     clearInterval(interval);
+    //   }
+    // }, 1000);
   }
+
 
   getFooterSliderMovies() {
     this.movieService.getFooterSliderMovies().subscribe(data => {
